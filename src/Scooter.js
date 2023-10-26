0dbadae926129d3fff6bd79c6161f8047a71c1c4
Andrew <andrw.smth@outlook.com>
2023-10-26 14:16:20 +0100

    user + scooter

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,59 @@
 class Scooter{
   // scooter code here
+static nextSerial = 1
+
+  constructor(station, user, serial, charge, isBroken){
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 10;
+    this.isBroken = false;
+  }
+
+
+
+  rent(user){
+    this.setCharge(this.charge)
+    if(this.charge > 20 && this.isBroken === false){
+     this.station = null;
+     this.user = user 
+     console.log(`${user} has rented ${this.serial} scooter`);
+    } else if (this.isBroken && this.charge < 20){
+      throw new Error('Scooter needs to recharge and repair')
+    } else if (this.isBroken){
+      throw new Error('Scooter is broken and needs to be repaired')
+    } else if(this.charge < 20){
+      throw new Error('Scooter needs to recharge');
+    }
+  }
+
+  dock(station){
+    this.user = null;
+    this.station = station
+    console.log(`Scooter has been docked at ${station}`);
+    console.log(`${this.user}`)
+  }
+
+
+  recharge() {
+    const interval = 1000
+
+    const rechargeInterval = setInterval(() => {
+      if (this.charge < 100) {
+        this.charge += 5;
+        console.log(`Charge increased to ${this.charge}%`)
+      } else {
+        clearInterval(rechargeInterval); 
+        console.log("Scooter fully charged.")
+      }
+    }, interval)
+  }
+
+
 }
+
+
+
 
 
 module.exports = Scooter
--- src/User.js
+++ src/User.js
@@ -1,5 +1,29 @@
 class User {
   // User code here
+  constructor(username, password, age, loggedIn){
+    this.username = username
+    this.password = password
+    this.age = age
+    this.loggedIn = false
+  }
+  login(enteredPassword){
+    if(enteredPassword === this.password){
+      this.loggedIn = true
+      console.log("user login successful")
+    }else{
+      throw new Error("Incorrect Password")
+    }
+  }
+
+  logout() {
+    this.loggedIn = false;
+    console.log("Logged out successfully.");
+  }
 }
+
+const u1 = new User("joe", "j123", 34)
+u1.login("j123")
+u1.logout()
+
 
 module.exports = User
